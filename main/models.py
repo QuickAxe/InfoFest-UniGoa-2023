24be0d8ad1863d24fbe70af5a80f1c91977e3b2e
TheInvincible95 <109205375+TheInvincible95@users.noreply.github.com>
2023-08-10 20:21:30 +0530

    Added dummy data

--- main/models.py
+++ main/models.py
@@ -13,11 +13,15 @@ class Teacher(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=200)
     passwd = models.CharField(max_length=50, default="password")
+    def __str__(self) -> str:
+        return f"{self.name} {self.email}"
  
 class Course(models.Model):
     'Course'
     name = models.CharField(max_length=50)
     Teacher = models.ForeignKey('Teacher', on_delete=models.PROTECT)
+    def __str__(self) -> str:
+        return f"{self.name}"
 
 
 class CourseContent(models.Model):
@@ -25,8 +29,12 @@ class CourseContent(models.Model):
     Course = models.ForeignKey('Course', on_delete=models.CASCADE)
     videoURL = models.URLField(max_length=200, default='example.com')
     textContent = models.CharField(max_length=500, default="lorem ipsum dolor set")
+    def __str__(self) -> str:
+        return f"{self.textContent}"
 
 class EnrollmentLog(models.Model):
     'This stores the log of a student enrolling into a course'
     Student = models.ForeignKey('Student', on_delete=models.CASCADE)
     Course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    def __str__(self) -> str:
+        return f"{self.Student.name} {self.Course}"
