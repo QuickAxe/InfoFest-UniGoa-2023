1c7fb386c79e171ad166d4f765387942f0dbc1ce
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2023-08-10 13:09:07 +0530

    adding api stuff

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from rest_framework.response import Response 
-from rest_framework import api_view 
+from rest_framework.decorators import api_view 
+
+
+@api_view(['GET'])
+def getData(request):
+    
+    # sample way to return json object as a get request 
+    # response 
+
+    person = {'name': 'hello', 'sirNAme':'world'}
+    
+    return Response(person)
+
 
